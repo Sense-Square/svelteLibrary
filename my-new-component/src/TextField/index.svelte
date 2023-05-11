ceb7a8fde1b2c4a82687f5d1df7a25494e88f7e9
Antonio <antoniopisu19@gmail.com>
2023-05-11 21:28:44 +0200

    cambio gestione focus and blur

--- my-new-component/src/TextField/index.svelte
+++ my-new-component/src/TextField/index.svelte
@@ -6,8 +6,6 @@
   export let value='';
   export let placeholder='';
   export let color='green';
-  export let focusColor='#f3f3df';
-  export let isFocused=false;
 
   function handleChange(event){
     value = event.target.value;
@@ -17,17 +15,6 @@
   function handleInput(event){
     value = event.target.value;
     dispatch("input", value);
-  }
-
-  function handleFocus(){
-    isFocused=true;
-    focusColor='#f3f3df';
-    dispatch('focus', {isFocused,focusColor});
-  }
-
-  function handleBlur(){
-    isFocused=false;
-    dispatch('blur', {isFocused, focusColor});
   }
 
   function handleKeyPress(event) {
@@ -43,15 +30,14 @@
   input{
     text-align: center;
   }
+
   input:focus{
     background-color: #f3f3df;
   }
 </style>
 
 <input type="text" bind:value={value} placeholder={placeholder}
-   style="color:{color}; background-color:{isFocused ? focusColor : ''};"
+   style="color:{color};"
    on:input
    on:change
-   on:focus
-   on:blur
    on:keypress />
