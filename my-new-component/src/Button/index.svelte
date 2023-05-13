881788053eb86710b92dc5d45d2a7e70f77348c1
Antonio <antoniopisu19@gmail.com>
2023-05-13 22:38:18 +0200

    aggiunta focus e hover

--- my-new-component/src/Button/index.svelte
+++ my-new-component/src/Button/index.svelte
@@ -1,36 +1,42 @@
 <script>
-    import {createEventDispatcher} from 'svelte';
-  
-    const dispatch = createEventDispatcher();
-  
-    export let label;
-  
-    function handleClick()
-    {
-      dispatch('message', {text: 'bottone premuto!'});
-    }
-  
-  </script>
-  <div class="b1">
-    <button on:click = {handleClick}>{label}</button>
-  </div>
-  
-  
-  <style>
-    .b1{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    button {
-      background-color: cornflowerblue;
-      color: white;
-      padding: 12px 24px;
-      font-size: medium;
-      margin: 4px 2px;
-      cursor: pointer;
-      border-radius: 8px;
-      
-    }
-  </style>
-  
+  import {createEventDispatcher} from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let label;
+
+  function handleClick()
+  {
+    dispatch('message', {text: 'bottone premuto!'});
+  }
+
+</script>
+<div class="b1">
+  <button on:click = {handleClick}>{label}</button>
+</div>
+
+
+<style>
+  .b1{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  button {
+    background-color: cornflowerblue;
+    color: white;
+    padding: 12px 24px;
+    font-size: medium;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 8px;
+    transition: background-color 0.8s ease;
+  }
+  button:hover{
+    background-color: #0b7dda;
+  }
+  button:focus{
+    outline: none;
+    background-color: #5F9EA0;
+  }
+</style>
