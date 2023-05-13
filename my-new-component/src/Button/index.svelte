24fc504070c24131447091f8bc0567783eea3b34
Antonio <antoniopisu19@gmail.com>
2023-05-13 22:11:26 +0200

    Aggiunta componente Button e on:click

---
+++ my-new-component/src/Button/index.svelte
@@ -0,0 +1,36 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+  
+    const dispatch = createEventDispatcher();
+  
+    export let label;
+  
+    function handleClick()
+    {
+      dispatch('message', {text: 'bottone premuto!'});
+    }
+  
+  </script>
+  <div class="b1">
+    <button on:click = {handleClick}>{label}</button>
+  </div>
+  
+  
+  <style>
+    .b1{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    button {
+      background-color: cornflowerblue;
+      color: white;
+      padding: 12px 24px;
+      font-size: medium;
+      margin: 4px 2px;
+      cursor: pointer;
+      border-radius: 8px;
+      
+    }
+  </style>
+  
