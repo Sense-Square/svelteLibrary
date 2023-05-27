e6ae819e7cd6d4d4c4fd6108507c37ef36ac5b04
Antonio <antoniopisu19@gmail.com>
2023-05-27 13:09:53 +0200

    css

--- my-new-component/src/index.js
+++ my-new-component/src/index.js
@@ -1,2 +1,3 @@
 export { default as TextField } from './TextField/index.svelte';
 export { default as Button } from './Button/index.svelte';
+export { default as MaterialApp } from './MaterialApp/index.svelte';
