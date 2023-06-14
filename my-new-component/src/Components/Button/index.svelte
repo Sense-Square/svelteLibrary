7f61cb02f04b8cd59349b3784b959806c795f8f7
Antonio <antoniopisu19@gmail.com>
2023-06-14 22:20:59 +0200

    css esterno per button e riorganizzaione cartelle

--- my-new-component/src/Components/Button/index.svelte
+++ my-new-component/src/Components/Button/index.svelte
@@ -1,4 +1,7 @@
 <script>
+
+  import './button.css';
+
   /**
    * Button contents
    * @type {string}
@@ -90,59 +93,3 @@
   {label}
   </button>
 
-
-
-<style>
-  .b1{
-    color: var(--md-sys-color-primary);
-    padding: 12px 24px;
-    margin: 4px 2px;
-    cursor: pointer;
-    border-radius: 20px;
-    transition: background-color 0.8s ease;
-    min-width: 120px;
-  }
-  .small {
-    font-size: 12px;
-  }
-
-  .medium {
-    font-size: 16px;
-  }
-
-  .big {
-    font-size: 24px;
-  }
-
-  .elevated {
-    background-color: var(--md-sys-color-primary-container);
-    color: white;
-    box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2);
-    border: 1px solid rgba(0, 0, 0, 0.2);
-  }
-
-  .filled {
-    background-color: var(--md-sys-color-primary-container);
-    color: white;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-  }
-
-  .tonal{
-    background-color: var(--md-sys-color-secondary-container);
-    color: whitesmoke;
-    border: 1px 
-    solid rgba(0, 0, 0, 0.2);
-  }
-
-  .outlined {
-    background-color: var(--md-sys-color-outline);
-    color: var(--md-sys-color-primary);
-    border: 1px solid var(--md-sys-color-primary);
-  }
-
-  .text {
-    background-color: transparent;
-    color: var(--md-sys-color-primary);
-    border: none;
-  }
-</style>
