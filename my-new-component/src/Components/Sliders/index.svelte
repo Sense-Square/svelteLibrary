c5d4cc4ec32ed4411fe6b3cac18cc2f042fe2cdf
Antonio <antoniopisu19@gmail.com>
2023-07-07 20:12:03 +0200

    basi per sliders

---
+++ my-new-component/src/Components/Sliders/index.svelte
@@ -0,0 +1,27 @@
+<script>
+import './sliders.css'
+
+    export let value = 0;
+    export let min;
+    export let max;
+    export let active = true;
+    export let disabled = false;
+
+    function handleChange(event){
+        value = event.target.value;
+    }
+
+</script>
+
+<div class="label-container">
+    <p class="label-text">Valore: {value}</p>
+  </div>
+<input 
+type="range" 
+min={min} 
+max={max} 
+disabled={disabled}
+bind:value 
+on:change
+on:update
+on:input={handleChange}/>
